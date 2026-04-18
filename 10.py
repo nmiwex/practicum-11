7953db9fe9b1fc87d09b100e84d85a2b785363f6
Timofeeva Veronika <114563249+nmiwex@users.noreply.github.com>
2026-04-18 12:38:01 +0700

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,7 +1,9 @@
-lst1 = list(input().split())
-lst2 = list(input().split())
+lst1 = list(map(int, input().split()))
+lst2 = list(map(int, input().split()))
 fr, to = map(int, input().split())
-for i in range(to, fr-1, -1):
+
+for i in range(to, fr - 1, -1):
     lst2.append(lst1.pop(i))
+    
 print(lst1)
 print(lst2)
